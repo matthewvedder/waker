3cc6c46af75b0bb52540bdebb9a0f4ad937ead02
Matthew Vedder <matthewvedder@gmail.com>
2018-12-02 09:07:31 -0500

    add font awesome

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,7 @@
 import React, { Component } from 'react'
 import Navbar from './Navbar'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import Divider from '@material-ui/core/Divider';
-import InboxIcon from '@material-ui/icons/Inbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 import '../styles/Sidebar.css'
 
 class Sidebar extends Component {
@@ -21,7 +15,9 @@ class Sidebar extends Component {
   render() {
     return (
       <div className='sidebar'>
-        <div className='sidebar-opac' />
+        <div className='sidebar-opac'>
+          <FontAwesomeIcon icon={faCoffee} />
+        </div>
       </div>
     )
   }
