38b46658918e4163c42dc368dc3c054db9a95173
Matthew Vedder <matthewvedder@gmail.com>
2018-12-21 22:25:23 -0500

    hook up sagas and actions in app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import { applyMiddleware, createStore, compose } from 'redux'
+import { Provider } from 'react-redux'
+import createSagaMiddleware from 'redux-saga'
 import PrivateRoute from './components/PrivateRoute'
 import Canvas from './components/Canvas'
 import LoginForm from './components/LoginForm'
 import Sidebar from './components/Sidebar'
 import { isAuthenticated } from './Auth'
+import RootReducer from './reducers'
+import RootSaga from './sagas'
 import './styles/App.css';
+
+const composeSetup = process.env.NODE_ENV !== 'production' && typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose
 
 class App extends Component {
   render() {
-      console.log(isAuthenticated())
+    const sagaMiddleware = createSagaMiddleware()
+    const store = createStore(
+      RootReducer,
+      composeSetup(applyMiddleware(sagaMiddleware)), // allows redux devtools to watch sagas
+    )
+    sagaMiddleware.run(RootSaga)
+
     return (
       <Router>
         <div className="App">
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
 import axios from 'axios'
 
 class LoginForm extends Component {
