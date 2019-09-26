a1bad8c4e6c22c765142c0064a779fc157b6c362
Matthew Vedder <matthewvedder@gmail.com>
2019-09-26 09:00:45 -0400

    create edit flow in asana sagas

--- src/actions/AsanaActions.js
+++ src/actions/AsanaActions.js
@@ -8,4 +8,4 @@ import {
 export const createAsana = (payload) => ({ type: CREATE_ASANA, payload })
 export const fetchAsanas = () => ({ type: FETCH_ASANAS })
 export const fetchAsana = (asanaId) => ({ type: FETCH_ASANA, asanaId })
-export const editAsana = (asanaId) => ({ type: EDIT_ASANA, asanaId })
+export const editAsana = (asanaId, payload) => ({ type: EDIT_ASANA, asanaId, payload })
--- src/sagas/AsanaSagas.js
+++ src/sagas/AsanaSagas.js
@@ -7,7 +7,8 @@ import {
   FETCH_ASANAS,
   FETCH_ASANA,
   SET_ASANAS,
-  SET_ASANA
+  SET_ASANA,
+  EDIT_ASANA
 } from '../actions/types'
 
 const url = `${process.env.REACT_APP_API_URL}/asanas`
@@ -59,6 +60,32 @@ function* getFlow (action) {
   }
 }
 
+function editApi (id, payload) {
+  return fetch(`${url}/${id}/edit`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload)
+  })
+    .then(handleApiErrors)
+    .then(response => response.json())
+    .then(json => json)
+    .catch((error) => { throw error })
+}
+
+function* editFlow (action) {
+  try {
+    const payload = yield call(editApi, action.asanaId, action.payload)
+    yield put({ type: ASANA_SUCCESS })
+    yield put({ type: SET_ASANA, payload })
+
+  } catch (error) {
+    console.warn(error)
+    yield put({ type: ASANA_ERROR, error })
+  }
+}
+
 
 function indexApi (payload) {
   return fetch(url, {
@@ -86,4 +113,5 @@ export function* watchAsanas() {
   yield takeLatest(CREATE_ASANA, createFlow)
   yield takeLatest(FETCH_ASANAS, indexFlow)
   yield takeLatest(FETCH_ASANA, getFlow)
+  yield takeLatest(EDIT_ASANA, editFlow)
 }
